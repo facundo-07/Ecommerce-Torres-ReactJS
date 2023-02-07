3d1741c34b1d3b86abe4aca708bdf2d73d263fd3
facundo torres <tfacundo108@gmail.com>
2023-02-07 19:43:58 -0300

    ....

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import NavBar from './components/Nav.jsx';
 import Title from './components/Title.jsx';
-// import Cart from './components/Cart.jsx';
 import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import {Home} from './components/Home.jsx';
 import MyCart from './components/Cart.jsx';
@@ -15,19 +14,19 @@ function App() {
         
         <Routes>
             
-            <Route exact path="/Preentrega1-Torres-ReactJS" index element={<Home title={'E-commerce'}/>}>
+            <Route exact path="/" element={<Home/>}>
             </Route>
 
-            <Route path="/Preentrega1-Torres-ReactJS/store" element={<MyCart.Cart/>}>
+            <Route path="/store" element={<MyCart.Cart/>}>
             </Route>
 
-            <Route path="/Preentrega1-Torres-ReactJS/store/smart-tvs" element={<MyCart.SmartCart/>}>
+            <Route path="/store/smart-tvs" element={<MyCart.SmartCart/>}>
             </Route>
 
-            <Route path="/Preentrega1-Torres-ReactJS/store/speakers" element={<MyCart.SpeakerCart/>}>
+            <Route path="/store/speakers" element={<MyCart.SpeakerCart/>}>
             </Route>
 
-            <Route path="/Preentrega1-Torres-ReactJS/store/mobile-phones" element={<MyCart.MobilesCart/>}>
+            <Route path="/store/mobile-phones" element={<MyCart.MobilesCart/>}>
             </Route>
 
             <Route path="/Preentrega1-Torres-ReactJS/store/laptops" element={<MyCart.LaptopCart/>}>
@@ -36,6 +35,7 @@ function App() {
             <Route path="/Preentrega1-Torres-ReactJS/store/earphones" element={<MyCart.EarphonesCart/>}>
             </Route>
         </Routes>
+        
         
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,5 +2,5 @@ import Title from './Title.jsx';
 import '../App.css';
 
 export function Home({title}){
-    return <Title text={title}/>
+    return <Title text={'E-commerce'}/>
 }
