ffa0143132c1f0b6ed400bcf0b5b08e0c2d3e7ea
facundo torres <tfacundo108@gmail.com>
2023-02-07 12:05:20 -0300

    trial2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div>
         <NavBar/>
         <Routes>
-            <Route path="/Ecommerce-Torres-ReactJS" element={<Home/>}>
+            <Route path="/" element={<Home/>}>
             </Route>
 
             <Route path="/store" element={<Cart/>}>
